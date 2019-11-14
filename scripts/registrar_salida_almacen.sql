3071f55b7e40730a11f89f348b7f2c924eaf2d0d
elvisiver12 <55523874+elvisiver12@users.noreply.github.com>
2019-11-14 14:31:07 -0500

    Correcciones de sp

--- scripts/registrar_salida_almacen.sql
+++ scripts/registrar_salida_almacen.sql
@@ -1,15 +1,20 @@
+USE `restaurante`;
+DROP procedure IF EXISTS `registrar_salida_almacen`;
+
+DELIMITER $$
+USE `restaurante`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `registrar_salida_almacen`(
-	IN `in_idInsumo` INTEGER,
-	IN `in_idAlmacenero` INTEGER, 
-	IN `in_cantidad` DECIMAL(8,2), 
-	IN `in_descripcion` TEXT
+    IN `in_idInsumo` INTEGER,
+    IN `in_idAlmacenero` INTEGER, 
+    IN `in_cantidad` DECIMAL(8,2), 
+    IN `in_descripcion` TEXT
  )
 BEGIN
-	/* 
-		Procedimiento que registra una salide del almacen, agregando una fila a
+    /* 
+        Procedimiento que registra una salide del almacen, agregando una fila a
         historial de almacen y modificando la tabla almacen.
          
-		RETORNA
+        RETORNA
         * 1 si se registro correctamente la entrada
         * 2 si no existe el insumo
         * 3 si no existe el almacenero o esta deshabilitado
@@ -27,41 +32,41 @@ BEGIN
     
     /* Verfica que exista el insumo */
     IF  in_idInsumo IS NULL
-		OR NOT EXISTS(
-			SELECT * FROM insumos 
+        OR NOT EXISTS(
+            SELECT * FROM insumos 
             WHERE idinsumo = in_idInsumo
             ) THEN
-		SET @existe_insumo = false;
-		SELECT 2 AS resultado;
+        SET @existe_insumo = false;
+        SELECT 2 AS resultado;
     END IF;
     
     /* Verfica que exista el almacener y este habilitada */
     IF  in_idAlmacenero IS NOT NULL
-		AND NOT EXISTS(
-			SELECT * FROM users 
+        AND NOT EXISTS(
+            SELECT * FROM users 
             WHERE id = in_idAlmacenero AND state = 1
             ) THEN
-		SET @existe_almacenero = false;
-		SELECT 3 AS resultado;
+        SET @existe_almacenero = false;
+        SELECT 3 AS resultado;
     END IF;
     
     /* Verifica que la cantidad sea valida */
     IF in_cantidad >= 0 THEN
-		SET @cantidad_valida = false;
+        SET @cantidad_valida = false;
         SELECT 4 AS resultado;
     END IF;    
     
      IF  in_cantidad IS NOT NULL
-		AND NOT EXISTS(
-			SELECT * FROM almacen 
+        AND NOT EXISTS(
+            SELECT * FROM almacen 
             WHERE (idinsumo = in_idInsumo)&&(cantidad + in_cantidad>=0)
             ) THEN
-		SET @cantidadSuficiente  = false;
-		SELECT 5 AS resultado;
+        SET @cantidadSuficiente  = false;
+        SELECT 5 AS resultado;
     END IF;
     
     IF @existe_insumo AND @existe_almacenero AND @cantidad_valida AND @cantidadSuficiente THEN
-		INSERT INTO historial_almacen (es_ajuste, idinsumo, idalmacenero, cantidad, descripcion)
+        INSERT INTO historial_almacen (es_ajuste, idinsumo, idalmacenero, cantidad, descripcion)
         VALUES (false, in_idInsumo, in_idAlmacenero, in_cantidad, in_descripcion);
         
         UPDATE almacen SET cantidad = cantidad + in_cantidad
@@ -71,4 +76,6 @@ BEGIN
     END IF;
     
     SET AUTOCOMMIT = 1;
-END
+END$$
+
+DELIMITER ;
